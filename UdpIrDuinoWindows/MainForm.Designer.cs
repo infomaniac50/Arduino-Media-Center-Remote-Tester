528b7c5f6f301d5a29836606549d175151d84d83
Derek Chafin <infomaniac50@gmail.com>
2013-12-14 21:31:07 -0600

    Added windows application to interact with the library.

---
+++ UdpIrDuinoWindows/MainForm.Designer.cs
@@ -0,0 +1,341 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: infomaniac50
+ * Date: 12/14/2013
+ * Time: 4:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace UdpIrDuino
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtBits = new System.Windows.Forms.TextBox();
+			this.txtEx1 = new System.Windows.Forms.TextBox();
+			this.txtEx2 = new System.Windows.Forms.TextBox();
+			this.txtValue = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.cmbType = new System.Windows.Forms.ComboBox();
+			this.btnSendCode = new System.Windows.Forms.Button();
+			this.txtBase2 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.txtBase16 = new System.Windows.Forms.TextBox();
+			this.txtBase10 = new System.Windows.Forms.TextBox();
+			this.txtBase8 = new System.Windows.Forms.TextBox();
+			this.btnDisconnect = new System.Windows.Forms.Button();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtBits
+			// 
+			this.txtBits.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBits.Location = new System.Drawing.Point(52, 172);
+			this.txtBits.Name = "txtBits";
+			this.txtBits.Size = new System.Drawing.Size(270, 20);
+			this.txtBits.TabIndex = 1;
+			// 
+			// txtEx1
+			// 
+			this.txtEx1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtEx1.Location = new System.Drawing.Point(52, 198);
+			this.txtEx1.Name = "txtEx1";
+			this.txtEx1.Size = new System.Drawing.Size(270, 20);
+			this.txtEx1.TabIndex = 2;
+			// 
+			// txtEx2
+			// 
+			this.txtEx2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtEx2.Location = new System.Drawing.Point(52, 224);
+			this.txtEx2.Name = "txtEx2";
+			this.txtEx2.Size = new System.Drawing.Size(270, 20);
+			this.txtEx2.TabIndex = 3;
+			// 
+			// txtValue
+			// 
+			this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtValue.Location = new System.Drawing.Point(52, 250);
+			this.txtValue.Name = "txtValue";
+			this.txtValue.Size = new System.Drawing.Size(270, 20);
+			this.txtValue.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 149);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(31, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Type";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 201);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(27, 13);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "EX1";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 227);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(27, 13);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "EX2";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 175);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(24, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Bits";
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 253);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(34, 13);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "Value";
+			// 
+			// cmbType
+			// 
+			this.cmbType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.cmbType.FormattingEnabled = true;
+			this.cmbType.Location = new System.Drawing.Point(52, 146);
+			this.cmbType.Name = "cmbType";
+			this.cmbType.Size = new System.Drawing.Size(270, 21);
+			this.cmbType.TabIndex = 0;
+			// 
+			// btnSendCode
+			// 
+			this.btnSendCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSendCode.Enabled = false;
+			this.btnSendCode.Location = new System.Drawing.Point(247, 276);
+			this.btnSendCode.Name = "btnSendCode";
+			this.btnSendCode.Size = new System.Drawing.Size(75, 23);
+			this.btnSendCode.TabIndex = 5;
+			this.btnSendCode.Text = "Send";
+			this.btnSendCode.UseVisualStyleBackColor = true;
+			// 
+			// txtBase2
+			// 
+			this.txtBase2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBase2.Location = new System.Drawing.Point(40, 19);
+			this.txtBase2.Name = "txtBase2";
+			this.txtBase2.Size = new System.Drawing.Size(264, 20);
+			this.txtBase2.TabIndex = 16;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+									| System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.txtBase16);
+			this.groupBox1.Controls.Add(this.txtBase10);
+			this.groupBox1.Controls.Add(this.txtBase8);
+			this.groupBox1.Controls.Add(this.txtBase2);
+			this.groupBox1.Location = new System.Drawing.Point(12, 13);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(310, 127);
+			this.groupBox1.TabIndex = 18;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Base Conversion";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(15, 100);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(19, 13);
+			this.label9.TabIndex = 23;
+			this.label9.Text = "16";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(15, 74);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(19, 13);
+			this.label8.TabIndex = 22;
+			this.label8.Text = "10";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(21, 48);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(13, 13);
+			this.label7.TabIndex = 21;
+			this.label7.Text = "8";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(21, 22);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(13, 13);
+			this.label6.TabIndex = 20;
+			this.label6.Text = "2";
+			// 
+			// txtBase16
+			// 
+			this.txtBase16.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBase16.Location = new System.Drawing.Point(40, 97);
+			this.txtBase16.Name = "txtBase16";
+			this.txtBase16.Size = new System.Drawing.Size(264, 20);
+			this.txtBase16.TabIndex = 19;
+			// 
+			// txtBase10
+			// 
+			this.txtBase10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBase10.Location = new System.Drawing.Point(40, 71);
+			this.txtBase10.Name = "txtBase10";
+			this.txtBase10.Size = new System.Drawing.Size(264, 20);
+			this.txtBase10.TabIndex = 18;
+			// 
+			// txtBase8
+			// 
+			this.txtBase8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBase8.Location = new System.Drawing.Point(40, 45);
+			this.txtBase8.Name = "txtBase8";
+			this.txtBase8.Size = new System.Drawing.Size(264, 20);
+			this.txtBase8.TabIndex = 17;
+			// 
+			// btnDisconnect
+			// 
+			this.btnDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnDisconnect.Enabled = false;
+			this.btnDisconnect.Location = new System.Drawing.Point(93, 276);
+			this.btnDisconnect.Name = "btnDisconnect";
+			this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+			this.btnDisconnect.TabIndex = 19;
+			this.btnDisconnect.TabStop = false;
+			this.btnDisconnect.Text = "Disconnect";
+			this.btnDisconnect.UseVisualStyleBackColor = true;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnConnect.Location = new System.Drawing.Point(12, 276);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(75, 23);
+			this.btnConnect.TabIndex = 20;
+			this.btnConnect.TabStop = false;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.btnSendCode;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(334, 311);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.btnDisconnect);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btnSendCode);
+			this.Controls.Add(this.cmbType);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtValue);
+			this.Controls.Add(this.txtEx2);
+			this.Controls.Add(this.txtEx1);
+			this.Controls.Add(this.txtBits);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(275, 340);
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.Text = "IrDuino";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.Button btnDisconnect;
+		private System.Windows.Forms.TextBox txtBase8;
+		private System.Windows.Forms.TextBox txtBase10;
+		private System.Windows.Forms.TextBox txtBase16;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox txtBase2;
+		private System.Windows.Forms.Button btnSendCode;
+		private System.Windows.Forms.ComboBox cmbType;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtValue;
+		private System.Windows.Forms.TextBox txtEx2;
+		private System.Windows.Forms.TextBox txtEx1;
+		private System.Windows.Forms.TextBox txtBits;
+	}
+}
---
+++ UdpIrDuinoWindows/MainForm.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: infomaniac50
+ * Date: 12/14/2013
+ * Time: 4:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Numerics;
+using UdpIrDuino.Utils;
+using System.Net;
+
+namespace UdpIrDuino
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		IPAddress ip;
+		IrCode code;
+		IrDuino ir;
+		long txtBaseNumber;
+
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			cmbType.Items.AddRange(Enum.GetNames(typeof(IrCodeType)));
+			
+			btnSendCode.Click += new EventHandler(btnSendCode_Click);
+			btnConnect.Click += new EventHandler(btnConnect_Click);
+			btnDisconnect.Click += new EventHandler(btnDisconnect_Click);
+			
+			txtBase2.Leave += new EventHandler(UpdateFromBinary);
+			txtBase8.Leave += new EventHandler(UpdateFromOctal);
+			txtBase10.Leave += new EventHandler(UpdateFromDecimal);
+			txtBase16.Leave += new EventHandler(UpdateFromHex);
+			
+			SetIpAddress("192.168.2.88");
+			
+			ir = new IrDuino(ip);
+		}
+
+		void UpdateConnectionStatus()
+		{
+			btnConnect.Enabled = !ir.IsConnected;
+			btnDisconnect.Enabled = ir.IsConnected;
+			btnSendCode.Enabled = ir.IsConnected;
+		}
+
+		void btnDisconnect_Click(object sender, EventArgs e)
+		{
+			ir.Disconnect();
+			
+			UpdateConnectionStatus();
+		}
+
+		void btnConnect_Click(object sender, EventArgs e)
+		{
+			ir.Connect();
+			
+			UpdateConnectionStatus();
+		}
+
+		bool SetIpAddress(string address)
+		{
+			return IPAddress.TryParse(address, out ip);
+		}
+
+		void UpdateToString()
+		{
+			txtBase2.Text = BaseConverter.ToString(txtBaseNumber, BaseConversions.Binary);
+			txtBase8.Text = BaseConverter.ToString(txtBaseNumber, BaseConversions.Octal);
+			txtBase10.Text = BaseConverter.ToString(txtBaseNumber, BaseConversions.Decimal);
+			txtBase16.Text = BaseConverter.ToString(txtBaseNumber, BaseConversions.Hexadecimal);
+		}
+
+		void UpdateFromBinary(object sender, EventArgs e)
+		{
+			if (txtBase2.Text.Length > 0 && BaseConverter.IsNumber(txtBase2.Text, BaseConversions.Binary)) {
+				txtBaseNumber = BaseConverter.FromString(txtBase2.Text, BaseConversions.Binary);
+				UpdateToString();
+			}
+		}
+
+		void UpdateFromOctal(object sender, EventArgs e)
+		{
+			if (txtBase8.Text.Length > 0 && BaseConverter.IsNumber(txtBase8.Text, BaseConversions.Octal)) {
+				txtBaseNumber = BaseConverter.FromString(txtBase8.Text, BaseConversions.Octal);
+				UpdateToString();
+			}
+		}
+
+		void UpdateFromDecimal(object sender, EventArgs e)
+		{
+			if (txtBase10.Text.Length > 0 && BaseConverter.IsNumber(txtBase10.Text, BaseConversions.Decimal)) {
+				txtBaseNumber = BaseConverter.FromString(txtBase10.Text, BaseConversions.Decimal);
+				UpdateToString();	
+			}
+		}
+
+		void UpdateFromHex(object sender, EventArgs e)
+		{
+			if (txtBase16.Text.Length > 0 && BaseConverter.IsNumber(txtBase16.Text, BaseConversions.Hexadecimal)) {
+				txtBaseNumber = BaseConverter.FromString(txtBase16.Text, BaseConversions.Hexadecimal);
+				UpdateToString();
+			}
+		}
+
+		void btnSendCode_Click(object sender, EventArgs e)
+		{
+			IrCodeType type = (IrCodeType)Enum.Parse(typeof(IrCodeType), cmbType.SelectedItem.ToString());
+			code = new IrCode();
+			code.type = (short)type;
+
+			if (short.TryParse(txtBits.Text, out code.nbits) && uint.TryParse(txtEx1.Text, out code.ex1) && uint.TryParse(txtEx2.Text, out code.ex2) && ulong.TryParse(txtValue.Text, out code.value)) {
+				ir.SendCode(code);
+			}
+		}
+
+		public void MakePacket()
+		{
+//			IrCode packet = new IrCode ();
+//			if (!MakePacket (client, ref packet)) {
+//				Console.WriteLine ("Invalid packet.");
+//				return;
+//			}
+		}
+		//
+		//		private bool MakePacket (UdpClient client, ref IrCode packet)
+		//		{
+		//			Console.Write ("Enter Code Type: ");
+		//			switch (Console.ReadLine ()) {
+		//				case "NEC":
+		//					packet.type = (Int16)IrCodeType.NEC;
+		//					break;
+		//				case "SONY":
+		//					packet.type = (Int16)IrCodeType.SONY;
+		//					break;
+		//				case "RC5":
+		//					packet.type = (Int16)IrCodeType.RC5;
+		//					break;
+		//				case "RC6":
+		//					packet.type = (Int16)IrCodeType.RC6;
+		//					break;
+		//				case "DISH":
+		//					packet.type = (Int16)IrCodeType.DISH;
+		//					break;
+		//				case "SHARP":
+		//					packet.type = (Int16)IrCodeType.SHARP;
+		//					break;
+		//				case "PANASONIC":
+		//					packet.type = (Int16)IrCodeType.PANASONIC;
+		//					break;
+		//				case "JVC":
+		//					packet.type = (Int16)IrCodeType.JVC;
+		//					break;
+		//				default:
+		//					Console.WriteLine("Not a valid code type");
+		//					return false;
+		//			}
+		//
+		//			Console.WriteLine ("Enter Bit Length");
+		//			if (!Int16.TryParse (Console.ReadLine (), out packet.nbits)) {
+		//				Console.WriteLine ("Not a valid number.");
+		//				return false;
+		//			}
+		//
+		//			packet.nbits = Math.Abs (packet.nbits);
+		//
+		//			Console.WriteLine("Enter ex1:");
+		//			if (!UInt32.TryParse(Console.ReadLine(), out packet.ex1))
+		//			{
+		//				Console.WriteLine("Not a valid number.");
+		//				return false;
+		//			}
+		//
+		//			Console.WriteLine("Enter ex2:");
+		//			if (!UInt32.TryParse(Console.ReadLine(), out packet.ex2))
+		//			{
+		//				Console.WriteLine("Not a valid number.");
+		//				return false;
+		//			}
+		//
+		//			Console.WriteLine ("Enter value:");
+		//			if (!UInt64.TryParse (Console.ReadLine (), out packet.value)) {
+		//				Console.WriteLine ("Not a valid number.");
+		//				return false;
+		//			}
+		//
+		//			return true;
+		//		}
+	}
+}
